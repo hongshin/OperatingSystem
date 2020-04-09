d2843d0f1751344f0c6212655e3607cf056a674e
Shin Hong <hongshin@handong.edu>
2020-04-09 11:47:00 +0900

    Create EC2.sh

---
+++ examples/EC2.sh
@@ -0,0 +1,3 @@
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-headers-4.15.0-041500_4.15.0-041500.201802011154_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-headers-4.15.0-041500-generic_4.15.0-041500.201802011154_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-image-4.15.0-041500-generic_4.15.0-041500.201802011154_amd64.deb
