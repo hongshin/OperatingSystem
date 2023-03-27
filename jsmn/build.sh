b34343bf06279547cc71dbfcc829249eb7c91718
hongshin <hongshin@gmail.com>
2023-03-27 13:36:16 +0900

    Revise build.sh
    Explicitly bind CC as clang.

--- jsmn/build.sh
+++ jsmn/build.sh
@@ -3,4 +3,4 @@
 set -x
 
 export ASAN_OPTIONS=detect_leaks=0:halt_on_error=1
-CFLAGS="-O0 -g -fsanitize=address" LDFLAGS="-fsanitize=address" make jsondump
+CC="clang" CFLAGS="-O0 -g -fsanitize=address" LDFLAGS="-fsanitize=address" make jsondump
